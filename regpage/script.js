981c5e175a8a043b653f2be82985ffa225831192
SAHIL DHOLPURIA <dholpuria1999@gmail.com>
2024-12-12 11:26:46 +0530

    Initial commit

---
+++ regpage/script.js
@@ -0,0 +1,4 @@
+document.getElementById('scholarship-form').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission for validation
+    alert('Your application has been submitted successfully!');
+  });
